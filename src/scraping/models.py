069d14beb4a49dfb87852d3b6f8a1dcb0de9368c
iLoveLadyGaga <prokyror21@gmail.com>
2020-09-28 15:53:53 +0300

    2st commit

---
+++ src/scraping/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50, verbose_name='City name')
+    slug = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        verbose_name = 'City name'
+        verbose_name_plural = 'City names'
+
+    def __str__(self):
+        return self.name
