9dacbe0c92da861526d7c1920b2fa05419ba1195
iLoveLadyGaga <prokyror21@gmail.com>
2020-09-28 18:23:39 +0300

    3

--- src/scraping/apps.py
+++ src/scraping/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ScrapingConfig(AppConfig):
     name = 'scraping'
-    verbose_name = 'scrapping application'
+    verbose_name = 'Scrapping application'
--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from .utils import cyrillic_to_latin
 
 
 class City(models.Model):
-    name = models.CharField(max_length=50, verbose_name='City name')
-    slug = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50,
+                            verbose_name='City name',
+                            unique=True)
+    slug = models.CharField(max_length=50,
+                            blank=True,
+                            unique=True)
 
     class Meta:
         verbose_name = 'City name'
@@ -11,3 +16,50 @@ class City(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = cyrillic_to_latin(str(self.name))
+        super().save(*args, **kwargs)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Programming language',
+                            unique=True)
+    slug = models.CharField(max_length=50,
+                            blank=True,
+                            unique=True)
+
+    class Meta:
+        verbose_name = 'Programming language'
+        verbose_name_plural = 'Programming languages'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = cyrillic_to_latin(str(self.name))
+        super().save(*args, **kwargs)
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250,
+                             verbose_name='Vacancy title')
+    company = models.CharField(max_length=250,
+                               verbose_name='Company name')
+    description = models.TextField(verbose_name='Description')
+    city = models.ForeignKey('City', on_delete=models.CASCADE,
+                             verbose_name='')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE,
+                                 verbose_name='Programming language')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Vacancy'
+        verbose_name_plural = 'Vacancies'
+
+    def __str__(self):
+        return self.title
