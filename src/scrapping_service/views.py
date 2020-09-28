c6f07ff084ece02d79b0a5efd7a924fee97dc9ae
iLoveLadyGaga <prokyror21@gmail.com>
2020-09-28 14:38:27 +0300

    1st commit

---
+++ src/scrapping_service/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+import datetime
+
+
+def home(request):
+    date = datetime.datetime.now().date()
+    name = 'LOX'
+    _context = {
+        'name': name,
+        'date': date
+    }
+    return render(request, 'home.html', _context)
